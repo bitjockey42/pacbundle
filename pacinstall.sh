3c2092bd0bae417cfa2ac9df82e62c90297f935b
hckr <hckr@eml.cc>
2014-05-17 11:02:27 -0500

    First version of the pacinstall script

---
+++ pacinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# script to install a list of packages specified in a plaintext Pacmanfile.
+
+install_packages () {
+    PACKAGES=$(awk '!/^#/ {printf $1 " "}' $1)
+    echo "Installing $PACKAGES"
+    pacman -S $PACKAGES
+}
+
+if [ -z "$1" ]
+then
+    echo "Usage: pacinstall /path/to/Pacmanfile"
+    exit 1
+else
+    install_packages $1
+fi
+
+
+
+
